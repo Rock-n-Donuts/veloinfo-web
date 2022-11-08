6d541825480db164dc9c9e0b8394cc1e97039f6f
Julien Carignan <carignan.julien@gmail.com>
2022-11-07 19:41:13 -0500

    markers hover map

--- src/components/partials/Map.jsx
+++ src/components/partials/Map.jsx
@@ -272,7 +272,7 @@ function Map({
             map.getView().on('change:center', onChangeCenter);
             map.getView().on('change:resolution', onChangeZoom);
             map.on('click', onMapClick);
-            // map.on('pointermove', onMapPointerMove);
+            map.on('pointermove', onMapPointerMove);
         }
 
         return () => {
@@ -280,7 +280,7 @@ function Map({
                 mapRef.current.getView().un('change:center', onChangeCenter);
                 mapRef.current.getView().un('change:resolution', onChangeZoom);
                 mapRef.current.un('click', onMapClick);
-                // mapRef.current.un('pointermove', onMapPointerMove);
+                mapRef.current.un('pointermove', onMapPointerMove);
 
                 mapContainer.innerHTML = '';
                 mapRef.current = null;
