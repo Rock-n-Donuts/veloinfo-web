b5081bcb11814c6232ed3e6debb5ed38c9d201f3
Julien Carignan <carignan.julien@gmail.com>
2022-12-30 17:18:15 -0500

    test fix

--- src/components/partials/ImageUpload.jsx
+++ src/components/partials/ImageUpload.jsx
@@ -55,7 +55,7 @@ function ImageUpload({ className, onChange, children }) {
             <input
                 ref={fileUploadRef}
                 type="file"
-                accept="capture=camera, image/heic, image/heif, image/jpeg, image/jpg, image/png"
+                accept="capture=camera, .heic, .heif, image/jpeg, image/jpg, image/png"
                 onChange={onChangePrivate}
             />
             <button type="button" className={styles.close} onClick={onCloseClick}>
