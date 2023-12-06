4c0ede884b77a312443bf3d03a5dc4b3039f6755
Julien Carignan <carignan.julien@gmail.com>
2023-12-05 20:34:26 -0500

    fix

--- src/components/partials/ImageUpload.jsx
+++ src/components/partials/ImageUpload.jsx
@@ -55,7 +55,7 @@ function ImageUpload({ className, onChange, children }) {
             <input
                 ref={fileUploadRef}
                 type="file"
-                accept="capture=camera,image/*"
+                accept="image/*;capture=camera"
                 capture
                 onChange={onChangePrivate}
             />
