321457b59fa61d617fc6634614d466a542d1ed8e
Julien Carignan <carignan.julien@gmail.com>
2023-12-08 12:59:07 -0500

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 
 import Container from './components/Container';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 import './styles/index.scss';
 
@@ -21,7 +21,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+// serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
